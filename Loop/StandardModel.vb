7e6b17b8f837c2b7f80fe4dcfdf70053e34b06ae
unknownsaying <15368104568@189.cn>
2026-02-15 16:18:50 +0800

    Update Down Quark mass from 4.7 to 4.8 MeV/c²

--- Loop/StandardModel.vb
+++ Loop/StandardModel.vb
@@ -49,7 +49,7 @@ Module ParticleData
         {Particle.UpQuark, New Info("Up Quark", "Quark", "+2/3", "1/2", "2.3 MeV/c²")},
         {Particle.CharmQuark, New Info("Charm Quark", "Quark", "+2/3", "1/2", "1.28 GeV/c²")},
         {Particle.TopQuark, New Info("Top Quark", "Quark", "+2/3", "1/2", "172.5 GeV/c²")},
-        {Particle.DownQuark, New Info("Down Quark", "Quark", "-1/3", "1/2", "4.7 MeV/c²")},
+        {Particle.DownQuark, New Info("Down Quark", "Quark", "-1/3", "1/2", "4.8 MeV/c²")},
         {Particle.StrangeQuark, New Info("Strange Quark", "Quark", "-1/3", "1/2", "95 MeV/c²")},
         {Particle.BottomQuark, New Info("Bottom Quark", "Quark", "-1/3", "1/2", "4.18 GeV/c²")},
                             
