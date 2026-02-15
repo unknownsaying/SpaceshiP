c98bb05d246f556e63da27d8fab8ca5514395b3d
unknownsaying <15368104568@189.cn>
2026-02-15 16:18:24 +0800

    Update StandardModel.vb

--- Loop/StandardModel.vb
+++ Loop/StandardModel.vb
@@ -46,12 +46,13 @@ End Class
 
 Module ParticleData
     Public ReadOnly InfoMap As New Dictionary(Of Particle, Info) From {
-        {Particle.UpQuark, New Info("Up Quark", "Quark", "+2/3", "1/2", "2.2 MeV/c²")},
+        {Particle.UpQuark, New Info("Up Quark", "Quark", "+2/3", "1/2", "2.3 MeV/c²")},
         {Particle.CharmQuark, New Info("Charm Quark", "Quark", "+2/3", "1/2", "1.28 GeV/c²")},
-        {Particle.TopQuark, New Info("Top Quark", "Quark", "+2/3", "1/2", "173.1 GeV/c²")},
+        {Particle.TopQuark, New Info("Top Quark", "Quark", "+2/3", "1/2", "172.5 GeV/c²")},
         {Particle.DownQuark, New Info("Down Quark", "Quark", "-1/3", "1/2", "4.7 MeV/c²")},
-        {Particle.StrangeQuark, New Info("Strange Quark", "Quark", "-1/3", "1/2", "96 MeV/c²")},
+        {Particle.StrangeQuark, New Info("Strange Quark", "Quark", "-1/3", "1/2", "95 MeV/c²")},
         {Particle.BottomQuark, New Info("Bottom Quark", "Quark", "-1/3", "1/2", "4.18 GeV/c²")},
+                            
         {Particle.Electron, New Info("Electron", "Lepton", "-1", "1/2", "0.511 MeV/c²")},
         {Particle.MuonLepton, New Info("Muon", "Lepton", "-1", "1/2", "105.7 MeV/c²")},
         {Particle.TauLepton, New Info("Tau", "Lepton", "-1", "1/2", "1.777 GeV/c²")},
@@ -59,6 +60,7 @@ Module ParticleData
         {Particle.MuonNeutrino, New Info("Muon Neutrino", "Lepton", "0", "1/2", "<0.17 MeV/c²")},
         {Particle.TauNeutrino, New Info("Tau Neutrino", "Lepton", "0", "1/2", "<18.2 MeV/c²")},
         {Particle.Photon, New Info("Photon", "Gauge Boson", "0", "1", "0")},
+                            
         {Particle.GluonBoson, New Info("Gluon", "Gauge Boson", "0", "1", "0")},
         {Particle.ZBoson, New Info("Z Boson", "Gauge Boson", "0", "1", "91.2 GeV/c²")},
         {Particle.WPlusBoson, New Info("W+ Boson", "Gauge Boson", "+1", "1", "80.4 GeV/c²")},
